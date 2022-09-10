c7ff09cebf00e28220c3a65e2379721e53005d05
DanielAzriel <danielaazrl@gmail.com>
2022-09-10 14:25:05 +0700

    Fix Register Error, add Register error message, Fix Register Layout

--- app/src/main/java/com/UGD/kurirku/register.kt
+++ app/src/main/java/com/UGD/kurirku/register.kt
@@ -3,40 +3,71 @@ package com.UGD.kurirku
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 
 class register : AppCompatActivity() {
-    private lateinit var nama: TextInputEditText
-    private lateinit var email: TextInputEditText
-    private lateinit var noHandphone : TextInputEditText
-    private lateinit var password: TextInputEditText
+    private lateinit var inputnama: TextInputLayout
+    private lateinit var inputemail: TextInputLayout
+    private lateinit var inputnoHandphone : TextInputLayout
+    private lateinit var inputpassword: TextInputLayout
     private lateinit var btnRegister: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
 
-        nama = findViewById(R.id.etNama)
-        email = findViewById(R.id.etEmail)
-        noHandphone = findViewById(R.id.etNo_Handphone)
-        password = findViewById(R.id.etPassword)
+        inputnama = findViewById(R.id.tilNama)
+        inputemail = findViewById(R.id.tilEmail)
+        inputnoHandphone = findViewById(R.id.tilNo_Handphone)
+        inputpassword = findViewById(R.id.tilPassword)
         btnRegister = findViewById(R.id.btnRegister)
 
-        btnRegister.setOnClickListener{
-            val mBundle = Bundle()
 
-            //Memasukan
-            mBundle.putString("nama", nama.text.toString())
-            mBundle.putString("email", email.text.toString())
-            mBundle.putString("noHandphone", noHandphone.text.toString())
-            mBundle.putString("password", password.text.toString())
-            //Intent dengan memanggil bundle
-            intent.putExtra("register",mBundle)
 
+        btnRegister.setOnClickListener(View.OnClickListener {
+            var checkLogin = false
+
+            val nama: String = inputnama.getEditText()?.getText().toString()
+            val email: String = inputemail.getEditText()?.getText().toString()
+            val noHanphone: String = inputnoHandphone.getEditText()?.getText().toString()
+            val password: String = inputpassword.getEditText()?.getText().toString()
+
+            if(nama.isEmpty()){
+                inputnama.setError("Nama must be filled!!")
+                checkLogin = false
+            }
+
+            if(email.isEmpty()){
+                inputemail.setError("Email must be filled!!")
+                checkLogin= false
+            }
+
+            if(noHanphone.isEmpty()){
+                inputnoHandphone.setError("No Handphone must be filled!!")
+                checkLogin=false
+            }else if(noHanphone.length <11){
+                inputnoHandphone.setError("Phone Number must contain 10 number or more ")
+                checkLogin = false
+            }
+
+
+            if(password.isEmpty()){
+                inputpassword.setError("Password must be filled!!")
+                checkLogin = false
+            }
+
+            if(nama.isNotEmpty() && email.isNotEmpty() && noHanphone.isNotEmpty() && password.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && noHanphone.isNotEmpty() && noHanphone.length>10) checkLogin = true
+            if(!checkLogin) return@OnClickListener
             val moveMain = Intent(this, MainActivity::class.java)
             startActivity(moveMain)
-        }
+
+        })
     }
 }
